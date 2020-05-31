bbc5ae0361689f8bcd077a7397cc65c42e9a114a
Philipp Albrecht <palbrecht@mailbox.org>
2020-05-31 12:01:53 +0200

    Add arch package lists and install script

---
+++ arch/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euf -o pipefail
+
+echo [-] installing packages from official repositories..
+sudo pacman -S --needed - < pkgs.txt
+
+echo [-] installing packages from AUR..
+while IFS='' read -r pkg_name; do
+    install_dir=$(mktemp -d)
+    git clone https://aur.archlinux.org/$pkg_name $install_dir
+    cd $install_dir
+    makepkg -sci
+    cd -
+done < "pkgs_aur.txt"
