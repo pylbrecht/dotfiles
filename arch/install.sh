2c7487bd6511902610acd46bf1a7a456ef4f649f
Philipp Albrecht <palbrecht@mailbox.org>
2020-06-01 10:03:03 +0200

    Don't require confirmation when installing packages

--- arch/install.sh
+++ arch/install.sh
@@ -2,13 +2,13 @@
 set -euf -o pipefail
 
 echo [-] installing packages from official repositories..
-sudo pacman -S --needed - < pkgs.txt
+sudo pacman -S --needed --noconfirm - < pkgs.txt
 
 echo [-] installing packages from AUR..
 while IFS='' read -r pkg_name; do
     install_dir=$(mktemp -d)
     git clone https://aur.archlinux.org/$pkg_name $install_dir
     cd $install_dir
-    makepkg -sci
+    makepkg -sci --noconfirm --needed
     cd -
 done < "pkgs_aur.txt"
