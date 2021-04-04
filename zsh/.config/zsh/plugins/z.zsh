62b4f9b85f833dfde6ffac99174a19306a8129ea
pylbrecht <pylbrecht@gmail.com>
2021-04-04 18:41:22 +0200

    Create directory for Z_DATA

--- zsh/.config/zsh/plugins/z.zsh
+++ zsh/.config/zsh/plugins/z.zsh
@@ -1,5 +1,13 @@
-_Z_DATA="$HOME/.cache/zsh/plugins/z/z_data"
+_Z_DATA_DIR="$HOME/.cache/zsh/plugins/z"
+
+if [ ! -d $_Z_DATA_DIR ] ; then
+    mkdir -p `dirname $_Z_DATA`
+    touch $_Z_DATA
+fi
+
+_Z_DATA="${_Z_DATA_DIR}/z_data"
 if [ ! -f $_Z_DATA ] ; then
     touch $_Z_DATA
 fi
+
 source "${0:h}/z/z.sh"
