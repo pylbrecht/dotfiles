bc707a8ee80ff37f2ab0e7fdb0ff24e2bf0a29f9
pylbrecht <palbrecht@mailbox.org>
2021-04-05 16:41:00 +0200

    Create Z_DATA_DIR if non-existent

--- zsh/.config/zsh/plugins/z.zsh
+++ zsh/.config/zsh/plugins/z.zsh
@@ -1,8 +1,7 @@
 _Z_DATA_DIR="$HOME/.cache/zsh/plugins/z"
 
 if [ ! -d $_Z_DATA_DIR ] ; then
-    mkdir -p `dirname $_Z_DATA`
-    touch $_Z_DATA
+    mkdir -p $_Z_DATA_DIR
 fi
 
 _Z_DATA="${_Z_DATA_DIR}/z_data"
