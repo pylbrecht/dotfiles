d1bedbc574690033733615e258d623a7d204a171
pylbrecht <palbrecht@mailbox.org>
2021-04-05 20:44:15 +0200

    Extract prompt.zsh plugin

---
+++ zsh/.config/zsh/plugins/prompt.zsh
@@ -0,0 +1,34 @@
+setopt promptsubst
+
+PROMPT='
+$(_user_host)%F{blue}%~%f
+%(!.%F{red}.%F{default})%f '
+
+PROMPT2=' '
+
+RPROMPT='%{$(echotc UP 1)%}$(_git_current_branch)%{$(echotc DO 1)%}'
+
+function _git_current_branch() {
+    local ref
+    ref=$(git symbolic-ref --quiet HEAD 2> /dev/null)
+    local ret=$?
+    if [[ $ret != 0 ]]; then
+        [[ $ret == 128 ]] && return  # no git repo.
+        ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+    fi
+    echo "שׂ %F{green}${ref#refs/heads/}%f"
+}
+
+function _user_host() {
+    local me
+
+    if [[ -n $SSH_CONNECTION ]]; then
+        me="%F{cyan}%n%f@%F{cyan}%m%f:"
+    elif [[ $LOGNAME != $USERNAME ]]; then
+        me="%F{cyan}%n%f:"
+    fi
+
+    if [[ -n $me ]] ; then
+        echo $me
+    fi
+}
