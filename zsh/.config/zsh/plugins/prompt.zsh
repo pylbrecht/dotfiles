b67d31757928bae0a300a67d5a26f9e25479c030
pylbrecht <pylbrecht@gmail.com>
2021-04-10 23:11:18 +0200

    Use vcs_info for git prompt

--- zsh/.config/zsh/plugins/prompt.zsh
+++ zsh/.config/zsh/plugins/prompt.zsh
@@ -1,23 +1,19 @@
 setopt promptsubst
 
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:git*' formats "שׂ %F{green}%b %u%c %f"
+zstyle ':vcs_info:git*' actionformats "שׂ %F{green}%b (%a)%f"
+precmd () { vcs_info }
+
+
 PROMPT='
 $(_user_host)%F{blue}%~%f
 %(!.%F{red}.%F{default})%f '
 
 PROMPT2=' '
 
-RPROMPT='%{$(echotc UP 1)%}$(_git_current_branch)%{$(echotc DO 1)%}'
-
-function _git_current_branch() {
-    local ref
-    ref=$(git symbolic-ref --quiet HEAD 2> /dev/null)
-    local ret=$?
-    if [[ $ret != 0 ]]; then
-        [[ $ret == 128 ]] && return  # no git repo.
-        ref=$(git rev-parse --short HEAD 2> /dev/null) || return
-    fi
-    echo "שׂ %F{green}${ref#refs/heads/}%f"
-}
+RPROMPT='%{$(echotc UP 1)%}${vcs_info_msg_0_}%{$(echotc DO 1)%}'
 
 function _user_host() {
     local me
