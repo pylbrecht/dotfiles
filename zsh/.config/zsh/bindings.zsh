c6e4e51e063f30ce2d89e319e771835ccef83437
pylbrecht <pylbrecht@gmail.com>
2021-05-24 08:06:55 +0200

    Remove duplicates in bindings.zsh

--- zsh/.config/zsh/bindings.zsh
+++ zsh/.config/zsh/bindings.zsh
@@ -1,5 +1,4 @@
 bindkey '\C-o' accept-line-and-down-history
-bindkey '\C-o' accept-line-and-down-history
 bindkey '^?' backward-delete-char
 bindkey '^a' beginning-of-line
 bindkey '^e' end-of-line
@@ -10,24 +9,13 @@ bindkey '^r' history-incremental-search-backward
 bindkey '^s' history-incremental-search-forward
 bindkey '^w' backward-kill-word
 bindkey -M menuselect '\C-o' accept-and-menu-complete
-bindkey -M menuselect '\C-o' accept-and-menu-complete
-bindkey -M menuselect '\e' accept-line
 bindkey -M menuselect '\e' accept-line
 bindkey -M menuselect '^M' .accept-line
-bindkey -M menuselect '^M' .accept-line
-bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
-bindkey -M menuselect 'j' vi-down-line-or-history
-bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
-bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'q' send-break
-bindkey -M menuselect 'q' send-break
-bindkey -M vicmd '\C-o' accept-line-and-down-history
-bindkey -M vicmd '\C-o' accept-line-and-down-history
-bindkey -M vicmd '\C-o' accept-line-and-down-history
 bindkey -M vicmd '\C-o' accept-line-and-down-history
 bindkey -M vicmd '^e' edit-command-line
 bindkey -M vicmd '^r' history-incremental-search-backward
