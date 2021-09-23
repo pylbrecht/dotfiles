48e64d2f098ca232e7bb44d87cbee02e570f9f33
Philipp Albrecht <palbrecht@mailbox.org>
2021-09-23 22:39:40 +0200

    Add zsh aliases for pacman

---
+++ zsh/.config/zsh/aliases/pacman.zsh
@@ -0,0 +1,6 @@
+alias pacupd='sudo pacman -Sy'
+alias pacupg='sudo pacman -Syu'
+alias pacin='sudo pacman -S'
+alias pacins='sudo pacman -U'
+alias pacre='sudo pacman -R'
+alias pacrem='sudo pacman -Rns'
