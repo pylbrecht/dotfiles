785a1d234b4ada72fcb8d39153dc666ca2d516f8
Philipp Albrecht <palbrecht@mailbox.org>
2021-10-28 20:35:36 +0200

    Play youtube via mpv

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,3 +1,6 @@
 config.load_autoconfig(True)
 c.content.blocking.whitelist = ["http://sentry.os.mm.local/solaris"]
 c.content.blocking.method = "both"
+
+config.bind(",m", "spawn mpv {url}")
+config.bind(",M", "hint links spawn mpv {hint-url}")
