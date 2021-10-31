0bb51366df16c24c523a627b673c034507a20e11
Philipp Albrecht <palbrecht@mailbox.org>
2021-10-31 02:23:04 +0200

    Set qutebrowser's editor to nvim

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -2,5 +2,7 @@ config.load_autoconfig(True)
 c.content.blocking.whitelist = ["http://sentry.os.mm.local/solaris"]
 c.content.blocking.method = "both"
 
+c.editor.command = ["alacritty", "--command", "nvim", "{file}", "-c", "normal {line}G{column0}l"]
+
 config.bind(",m", "spawn mpv {url}")
 config.bind(",M", "hint links spawn mpv {hint-url}")
