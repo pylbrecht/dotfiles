20b76e558d2b09fcbd903555fb2898404e9d8f9a
Philipp Albrecht <palbrecht@mailbox.org>
2022-03-19 13:00:40 +0100

    Add text objects and surround to zsh vi mode
    https://thevaluable.dev/zsh-install-configure-mouseless/

--- zsh/.config/zsh/plugins/vi-mode.zsh
+++ zsh/.config/zsh/plugins/vi-mode.zsh
@@ -34,3 +34,27 @@ export KEYTIMEOUT=5
 # allow ctrl-e to edit the command line
 autoload -Uz edit-command-line
 zle -N edit-command-line
+
+# text objects (https://thevaluable.dev/zsh-install-configure-mouseless/)
+autoload -Uz select-bracketed select-quoted
+zle -N select-quoted
+zle -N select-bracketed
+for km in viopp visual; do
+    bindkey -M $km -- '-' vi-up-line-or-history
+    for c in {a,i}${(s..)^:-\'\"\`\|,./:;=+@}; do
+        bindkey -M $km $c select-quoted
+    done
+    for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+        bindkey -M $km $c select-bracketed
+    done
+done
+
+# surround (https://thevaluable.dev/zsh-install-configure-mouseless/)
+autoload -Uz surround
+zle -N delete-surround surround
+zle -N add-surround surround
+zle -N change-surround surround
+bindkey -M vicmd cs change-surround
+bindkey -M vicmd ds delete-surround
+bindkey -M vicmd ys add-surround
+bindkey -M visual S add-surround
