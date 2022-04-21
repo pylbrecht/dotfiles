9d1e2c36823ff2ae94cb4bb77592697b4957a102
Philipp Albrecht <palbrecht@mailbox.org>
2022-04-21 13:00:55 +0200

    Update zsh global aliases

--- zsh/.config/zsh/aliases/global.zsh
+++ zsh/.config/zsh/aliases/global.zsh
@@ -1,6 +1,9 @@
-alias -g V=' | xargs -o vim'
-alias -g L=' | less -r'
+alias -g V=' | xargs nvim'
+alias -g L=' | less'
+alias -g LC='--color=always | less -r'
 alias -g C=' | xclip -selection clipboard'
+alias -g X=' | xargs'
+alias -g F=' | fzf'
 
 # taken from zsh-lovers
 alias -g ...='../..'
