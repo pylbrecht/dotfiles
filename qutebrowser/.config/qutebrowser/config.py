ade3448e3ba61c5dffe01c4f33c855b43f5f3c7b
Philipp Albrecht <palbrecht@mailbox.org>
2022-04-23 06:44:03 +0200

    Add nord color palette to qutebrowser config

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -10,3 +10,22 @@ config.bind(",M", "hint links spawn mpv {hint-url}")
 # bitwarden auto-fill (v stands for vault)
 config.bind(",vv", "spawn --userscript qute-bitwarden --auto-lock 300")
 config.bind(",vp", "spawn --userscript qute-bitwarden --password-only --auto-lock 300")
+
+nord = {
+    "nord0": "#2e3440",
+    "nord1": "#3b4252",
+    "nord2": "#434c5e",
+    "nord3": "#4c566a",
+    "nord4": "#d8dee9",
+    "nord5": "#e5e9f0",
+    "nord6": "#eceff4",
+    "nord7": "#8fbcbb",
+    "nord8": "#88c0d0",
+    "nord9": "#81a1c1",
+    "nord10": "#5e81ac",
+    "nord11": "#bf616a",
+    "nord12": "#d08770",
+    "nord13": "#ebcb8b",
+    "nord14": "#a3be8c",
+    "nord15": "#b48ead",
+}
