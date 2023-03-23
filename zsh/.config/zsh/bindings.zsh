91a2fd8a71eb0820113f11e3889b9640d0fe328a
Philipp Albrecht <palbrecht@mailbox.org>
2023-03-23 08:45:53 +0100

    Add tmux-sessionizer from ThePrimeagen

--- zsh/.config/zsh/bindings.zsh
+++ zsh/.config/zsh/bindings.zsh
@@ -20,3 +20,8 @@ bindkey -M menuselect 'q' send-break
 bindkey -M vicmd '\C-o' accept-line-and-down-history
 bindkey -M vicmd '^e' edit-command-line
 bindkey -M vicmd '^r' history-incremental-search-backward
+
+_tmux-sessionizer() tmux-sessionizer
+zle -N _tmux-sessionizer
+bindkey '^g' _tmux-sessionizer
+
