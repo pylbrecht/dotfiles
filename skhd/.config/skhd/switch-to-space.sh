6c3b50f6c8da8e92ced5671390a1d60a3ce91b49
pylbrecht <pylbrecht@mailbox.org>
2023-10-05 09:37:36 +0200

    Fix yabai switch-to-space script
    I had some logic issue there, but now it's working properly.

--- skhd/.config/skhd/switch-to-space.sh
+++ skhd/.config/skhd/switch-to-space.sh
@@ -4,19 +4,18 @@
 # (https://i3wm.org/docs/userguide.html#workspace_auto_back_and_forth)
 
 RECENT_SPACE_FILE="/tmp/yabai-recent-space-index"
-
 ACTIVE_SPACE=$(yabai -m query --spaces --display | jq '.[] | select(."has-focus" == true).index')
 
-if [ -f "${RECENT_SPACE_FILE}" ]; then
-	RECENT_SPACE=$(cat ${RECENT_SPACE_FILE})
+if [ ! -f "$RECENT_SPACE_FILE" ]; then
+	RECENT_SPACE=$ACTIVE_SPACE
 else
-	RECENT_SPACE=${ACTIVE_SPACE}
+	RECENT_SPACE=$(cat "$RECENT_SPACE_FILE")
 fi
 
-echo $RECENT_SPACE > ${RECENT_SPACE_FILE}
-
 if [ "$ACTIVE_SPACE" == $1 ]; then
-	skhd -k "ctrl - ${RECENT_SPACE}"
+	skhd -k "ctrl - $RECENT_SPACE"
 else
 	skhd -k "ctrl - $1"
 fi
+
+echo $ACTIVE_SPACE > "$RECENT_SPACE_FILE"
