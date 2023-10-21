96d2f79d49bdc44350aea9658367e1abbac2a7a9
pylbrecht <pylbrecht@mailbox.org>
2023-10-21 09:41:01 +0200

    Add rust-tools.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -32,6 +32,7 @@ require('lazy').setup({
   'nvim-lua/plenary.nvim',
   'nvim-lualine/lualine.nvim',
   'rust-lang/rust.vim',
+  'simrat39/rust-tools.nvim',
   'tpope/vim-abolish',
   'tpope/vim-commentary',
   'tpope/vim-eunuch',
@@ -77,6 +78,41 @@ require('lazy').setup({
   },
 
   { 'folke/which-key.nvim', opts = {} },
+
+  {
+    'simrat39/rust-tools.nvim',
+    opts = {
+        tools = {
+          runnables = {
+            use_telescope = true,
+          },
+        inlay_hints = {
+          auto = true,
+          show_parameter_hints = false,
+          parameter_hints_prefix = "",
+          other_hints_prefix = "",
+        },
+      },
+
+      -- all the opts to send to nvim-lspconfig
+      -- these override the defaults set by rust-tools.nvim
+      -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+      server = {
+        -- on_attach is a callback called when the language server attachs to the buffer
+        on_attach = on_attach,
+        settings = {
+          -- to enable rust-analyzer settings visit:
+          -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+          ["rust-analyzer"] = {
+            -- enable clippy on save
+            checkOnSave = {
+              command = "clippy",
+            },
+          },
+        },
+      },
+    }
+  },
 
   {
     'lewis6991/gitsigns.nvim',
