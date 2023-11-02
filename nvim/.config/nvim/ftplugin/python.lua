02c044ad2bb30d6b1fcd08635f1fdea0546360c1
pylbrecht <pylbrecht@mailbox.org>
2023-11-02 13:23:11 +0100

    Extend black-macchiato mappings
    Adding mappings for
    * formatting the current line
    * formatting the entire file
    
    Also making the mappings silent.

--- nvim/.config/nvim/ftplugin/python.lua
+++ nvim/.config/nvim/ftplugin/python.lua
@@ -1,2 +1,3 @@
-vim.api.nvim_set_keymap("v", "<leader>p", ":'<'>!black-macchiato<cr>", {})
-vim.api.nvim_set_keymap("n", "<leader>p", ":.!black-macchiato<cr>", {})
+vim.api.nvim_set_keymap("v", "<leader>p", ":'<'>!black-macchiato<cr>", {silent = true})
+vim.api.nvim_set_keymap("n", "<leader>pp", ":.!black-macchiato<cr>", {silent = true})
+vim.api.nvim_set_keymap("n", "<leader>pa", ":!black %<cr>", {silent = true})
