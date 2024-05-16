71e3360575b95ee2f669fdf91f944824823ce4fd
pylbrecht <pylbrecht@mailbox.org>
2024-05-16 21:45:48 +0200

    fix(nvim): remove LSP diagnostic mappings
    Since nvim v0.10.0 these are builtin.

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -80,5 +80,3 @@ vim.keymap.set("n", "<leader>lr", ":LspRestart<cr>", {silent = true})
 
 -- Navigate diagnostics
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { noremap = true, silent = true })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { noremap = true, silent = true })
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { noremap = true, silent = true })
