2f6160480f24ec839b79228a40233fdd299bd7a2
pylbrecht <pylbrecht@mailbox.org>
2024-05-17 09:42:08 +0200

    refactor(nvim): clean up completion
    see https://www.youtube.com/watch?v=22mrSjknDHI

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -85,11 +85,17 @@ require('lazy').setup({
   {
     'hrsh7th/nvim-cmp',
     dependencies = {
-      'L3MON4D3/LuaSnip',
+      'onsails/lspkind.nvim',
+      { 'L3MON4D3/LuaSnip', build = "make install_jsregexp" },
       'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
       'rafamadriz/friendly-snippets',
     },
+    config = function()
+      require "completion"
+    end,
   },
 
   { 'folke/which-key.nvim', opts = {} },
@@ -268,36 +274,6 @@ require("mason-lspconfig").setup({
   automatic_installation = true
 })
 require("nvim-surround").setup()
--- [[ Configure nvim-cmp ]]
--- See `:help cmp`
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
-    ['<C-y>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-}
-
 require("ibl").setup({
   indent = {
     char = '┊',
---
+++ nvim/.config/nvim/lua/completion.lua
@@ -0,0 +1,44 @@
+local lspkind = require "lspkind"
+lspkind.init {}
+
+require("luasnip.loaders.from_vscode").lazy_load()
+local luasnip = require "luasnip"
+
+vim.keymap.set({"i", "s"}, "<C-J>", function()
+  if luasnip.expand_or_jumpable then
+    luasnip.expand_or_jump()
+  end
+end, {silent = true})
+
+vim.keymap.set({"i"}, "<C-k>", function() 
+  if luasnip.jumpable(-1) then
+    luasnip.jump(-1)
+  end
+end, {silent = true})
+
+local cmp = require "cmp"
+cmp.setup {
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+  },
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert {
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<C-y>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+  },
+}
+
