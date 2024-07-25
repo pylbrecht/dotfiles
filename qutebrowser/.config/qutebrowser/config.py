c1b16de7be5d14800a95fe637c7c817440806466
pylbrecht <pylbrecht@mailbox.org>
2024-07-25 13:46:15 +0200

    fix: set PATH in /etc/paths

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -9,9 +9,6 @@ config.load_autoconfig(True)
 c.content.blocking.whitelist = ["https://app.datadoghq.eu/account/saml/assertion"]
 c.content.blocking.method = "both"
 
-if sys.platform == "darwin":
-    os.environ["PATH"] += ":/opt/homebrew/bin"
-
 c.editor.command = ["kitty", "nvim", "{file}", "-c", "normal {line}G{column0}l"]
 c.tabs.padding = {'bottom': 4, 'left': 5, 'right': 5, 'top': 4}
 c.tabs.indicator.padding = {'bottom': 0, 'left': 0, 'right': 4, 'top': 0}
