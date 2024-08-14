f1331892daca78d9590cf320d0e4d4dbad138e7f
pylbrecht <pylbrecht@mailbox.org>
2024-08-14 10:52:42 +0200

    feat(wezterm): add rosepine theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,8 @@
-local wezterm = require 'wezterm'
+local wezterm = require('wezterm')
+local rosepine = wezterm.plugin.require('https://github.com/neapsix/wezterm')
+
 local config = {}
+
+config.colors = rosepine.moon.colors()
 
 return config
