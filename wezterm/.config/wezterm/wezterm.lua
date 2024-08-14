ff9344723313b3c1c0e4b8f9f0081533f8c34ca9
pylbrecht <pylbrecht@mailbox.org>
2024-08-14 15:07:33 +0200

    feat(wezterm): add window padding

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,5 +8,11 @@ config.font = wezterm.font("SauceCodePro Nerd Font")
 config.font_size = 19
 
 config.enable_tab_bar = false
+config.window_padding = {
+  left = "8px",
+  right = "8px",
+  top = 0,
+  bottom = 0,
+}
 
 return config
