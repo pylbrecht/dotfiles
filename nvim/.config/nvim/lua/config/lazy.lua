c8bba4f235d4a3ea8839b89e9f151dca12c05706
pylbrecht <pylbrecht@mailbox.org>
2024-11-06 15:03:11 +0100

    refactor(nvim): use structured setup for lazy

---
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -0,0 +1,233 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.user_emmet_leader_key = '<C-e>'
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  'will133/vim-dirdiff',
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+  },
+  {
+    "julienvincent/hunk.nvim",
+    cmd = { "DiffEditor" },
+    config = function()
+      require("hunk").setup({
+        ui = {
+          layout = 'horizontal',
+        },
+      })
+    end,
+  },
+  {
+    'ruifm/gitlinker.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  {
+    "folke/ts-comments.nvim",
+    opts = {},
+    event = "VeryLazy",
+    enabled = vim.fn.has("nvim-0.10.0") == 1,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function ()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+      require("copilot_cmp").setup()
+    end
+  },
+  {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  {
+    "andymass/vim-matchup",
+    config = function()
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+      require("nvim-treesitter.configs").setup({
+        matchup = {
+          enable = true,
+          disable_virtual_text = true,
+        },
+      })
+    end
+  },
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {
+      disabled_filetypes = {'tagbar', 'fugitive', 'fugitiveblame', 'mason'},
+      max_count = 3,
+      disabled_keys = {
+        ["<Up>"] = {},
+        ["<Down>"] = {},
+      },
+    }
+  },
+  { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} },
+  {
+    "williamboman/mason.nvim",
+    "mfussenegger/nvim-dap",
+    "jay-babu/mason-nvim-dap.nvim",
+  },
+  'mfussenegger/nvim-dap',
+  'mfussenegger/nvim-dap-python',
+  'theHamsta/nvim-dap-virtual-text',
+  'caenrique/nvim-maximize-window-toggle',
+  'dhruvasagar/vim-zoom',
+  'nvim-tree/nvim-web-devicons',
+  'kylechui/nvim-surround',
+  'majutsushi/tagbar',
+  'mattn/emmet-vim',
+  'nvim-lua/plenary.nvim',
+  'nvim-lualine/lualine.nvim',
+  'rust-lang/rust.vim',
+  'tpope/vim-abolish',
+  'tpope/vim-eunuch',
+  'tpope/vim-fugitive',
+  'tpope/vim-markdown',
+  'tpope/vim-repeat',
+  'tpope/vim-sensible',
+  'tpope/vim-sleuth',
+  'tpope/vim-unimpaired',
+  'vim-test/vim-test',
+  'MunifTanjim/prettier.nvim',
+
+  { 'echasnovski/mini.nvim', version = false },
+
+  { 'rose-pine/neovim', name = 'rose-pine' },
+
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  { 'fatih/vim-go', build = ':GoUpdateBinaries' },
+
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      { 'williamboman/mason.nvim', config = true },
+      'williamboman/mason-lspconfig.nvim',
+      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+      'folke/neodev.nvim',
+    },
+  },
+
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'onsails/lspkind.nvim',
+      { 'L3MON4D3/LuaSnip', build = "make install_jsregexp" },
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
+      'rafamadriz/friendly-snippets',
+    },
+    config = function()
+      require "completion"
+    end,
+  },
+
+  { 'folke/which-key.nvim', opts = {} },
+
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      on_attach = function(bufnr)
+        vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
+
+        -- don't override the built-in and fugitive keymaps
+        local gs = package.loaded.gitsigns
+        vim.keymap.set({'n', 'v'}, ']c', function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gs.next_hunk() end)
+          return '<Ignore>'
+        end, {expr=true, buffer = bufnr, desc = "Jump to next hunk"})
+        vim.keymap.set({'n', 'v'}, '[c', function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gs.prev_hunk() end)
+          return '<Ignore>'
+        end, {expr=true, buffer = bufnr, desc = "Jump to previous hunk"})
+      end,
+    },
+  },
+
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'rose-pine',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+  { 'lukas-reineke/indent-blankline.nvim', main = "ibl", opts = {} },
+  -- Fuzzy Finder (files, lsp, etc)
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+      -- Only load if `make` is available. Make sure you have the system
+      -- requirements installed.
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    },
+  },
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      'nvim-treesitter/nvim-treesitter-context',
+    },
+    build = ':TSUpdate',
+  },
+}, {})
