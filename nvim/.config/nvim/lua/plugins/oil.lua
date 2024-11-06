14e2ab232aa352dc8de002f698eaff187ba353ec
pylbrecht <pylbrecht@mailbox.org>
2024-11-06 15:03:11 +0100

    feat(nvim): customize Oil keymaps

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -2,14 +2,19 @@ return {
   {
     'stevearc/oil.nvim',
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
+    opts = {
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        ["<CR>"] = "actions.select",
+        ["<C-v>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-x>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-l>"] = "actions.refresh",
+      },
+    },
       local oil = require("oil")
-
-      oil.setup({
-        view_options = {
-          show_hidden = true,
-        },
-      })
+      oil.setup(opts)
 
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end,
