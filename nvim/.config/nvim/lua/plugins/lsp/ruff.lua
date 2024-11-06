cafa34ea12710b1305cf8ed87d1e659f8384d35f
pylbrecht <pylbrecht@mailbox.org>
2024-11-06 15:03:11 +0100

    fix(nvim): prevent ruff from attaching multiple times

--- nvim/.config/nvim/lua/plugins/lsp/ruff.lua
+++ nvim/.config/nvim/lua/plugins/lsp/ruff.lua
@@ -1,4 +1,11 @@
 return {
+  root_markers = {
+    'pyproject.toml',
+    -- We have multiple ruff.toml files in the project, which leads to ruff attaching multiple times
+    -- 'ruff.toml',
+    '.ruff.toml',
+    '.git'
+  },
   on_attach = function(client, bufnr)
     if client.name == 'ruff' then
       -- Disable hover in favor of Pyright
