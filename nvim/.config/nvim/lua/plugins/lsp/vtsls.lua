2c8d0d5a6a7b4545dcdd81b866864b01cba1e2ae
pylbrecht <pylbrecht@mailbox.org>
2024-11-06 15:03:11 +0100

    fix(nvim): vuejs lsp setup
    No more weird, unintended formatting.

--- nvim/.config/nvim/lua/plugins/lsp/vtsls.lua
+++ nvim/.config/nvim/lua/plugins/lsp/vtsls.lua
@@ -1,14 +1,25 @@
-local vue_language_server_path = vim.fn.expand '$MASON/packages' ..
-'/vue-language-server' .. '/node_modules/@vue/language-server'
+local vue_language_server_path = vim.fn.stdpath("data")
+    .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 
 local vue_plugin = {
-  name = '@vue/typescript-plugin',
+  name = "@vue/typescript-plugin",
   location = vue_language_server_path,
-  languages = { 'vue' },
-  configNamespace = 'typescript',
+  languages = { "vue" },
+  configNamespace = "typescript",
 }
+
 return {
   settings = {
+    javascript = {
+      format = {
+        enable = false,
+      },
+    },
+    typescript = {
+      format = {
+        enable = false,
+      },
+    },
     vtsls = {
       tsserver = {
         globalPlugins = {
@@ -17,5 +28,5 @@ return {
       },
     },
   },
-  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
 }
