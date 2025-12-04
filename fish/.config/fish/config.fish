8d7bc0eee1a38d71d93e8ebc39648e78113bb4b0
pylbrecht <pylbrecht@mailbox.org>
2025-12-04 16:48:10 +0100

    fix(fish): export GEM_HOME

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -45,6 +45,8 @@ jj util completion fish | source
 fish_add_path --universal --prepend ~/.cargo/bin
 set --export --universal EDITOR nvim
 set --export --universal MANPAGER "nvim +Man!"
+set --export --universal GEM_HOME (gem env user_gemhome)
+fish_add_path $GEM_HOME/bin
 
 abbr -a --position anywhere C "| fish_clipboard_copy"
 
