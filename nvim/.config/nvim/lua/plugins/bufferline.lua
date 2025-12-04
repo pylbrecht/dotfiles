2825cc7d170fffd2d7bac5a109769f6d5e7af468
pylbrecht <pylbrecht@mailbox.org>
2025-12-04 16:48:10 +0100

    fix(nvim): unapplied opts in bufferline

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -3,18 +3,12 @@ return {
     'akinsho/bufferline.nvim',
     version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons',
-    opts = {
-      options = {
-        mode = "tabs",
-        always_show_bufferline = false,
-        show_buffer_close_icons = false,
-      }
-    },
     config = function()
       vim.opt.termguicolors = true
       local bufferline = require('bufferline')
       bufferline.setup({
         options = {
+          mode = "tabs",
           always_show_bufferline = false,
           show_buffer_close_icons = false,
           show_tab_indicators = false,
